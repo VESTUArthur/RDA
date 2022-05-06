a044aa343f889d0b98c359b1b6037b4f2628b8a8
VESTUArthur <vestuarthur@gmail.com>
2022-05-06 14:10:59 +0200

    Docstring added

--- code_examples/Rcode.R
+++ code_examples/Rcode.R
@@ -54,3 +54,31 @@ write.csv(data, "{filename}")
 
 meta2d("C:/Users/arthu/Documents/GitHub/RDA/formated_spreadsheet.txt",
     timepoints = "line1", filestyle = "txt")
+
+
+
+library(TimeCycle)
+library(rain)
+data(menetRNASeqMouseLiver)
+data <- menetRNASeqMouseLiver
+rownames(data) <- NULL
+write.csv(data, "data.csv")
+data <- read.csv("data.csv")
+head(data)
+data$X <- NULL
+res <- rain(t(data), deltat = 4, nr.series = 2, period = 24, verbose = TRUE)
+TimeCycleResults <- TimeCycle(data = t(data), repLabel = rep(1, 24))
+write.csv(res, "RAINresult_")
+plot(res$pVal)
+print(df)
+
+data <- zhang2014
+
+TimeCycleResults <- TimeCycle(data = data, repLabel = rep(1,24))
+
+library(TimeCycle)
+
+#set seed for reproducibility with random variables in example usage
+set.seed(1234)
+TimeCycleResults <- TimeCycle(data = zhang2014, repLabel = rep(1,24))
+TimeCycleResults
